4025d4ebed3de493f605e2d7156cbe3edbdbd2d6
Farhin Chowdhury <farhinchowdhury0@gmail.com>
2020-10-09 23:15:34 -0400

    added badge and updated function to look cleaner

--- Develop/index.js
+++ Develop/index.js
@@ -58,64 +58,73 @@ const questions = [
     }
 
 ];
+//array for all the user inputs
 let userList =[]
+
+
+
 // function to write README file
 
-
-
 function writeToFile(data) {
-    let descriptionData = `## Description:\n ${data[0].description}\n\n\n`
-    
+    let descriptionData = `## Description:\n ${data.description}\n\n\n`  
     let tableofConents = `## Table of Content:\n\n* [Installation](*installation)\n* [Usage](#usage)\n* [License](#license)\n* [Contributing](#contributing)\n* [Tests](#tests)\n* [Questions](#questions)\n\n\n`
-    
     let installData = `## Installation:\n\n To install necessary dependencies, run the following command:\n\n\n
-    ${data[0].install}\n\n\n`
-    let usageData = `## Usage:\n\n${data[0].repo1}\n\n\n`
+    ${data.install}\n\n\n`
     
-    let contributingData = `## Contributing:\n\n${data[0].repo2}\n\n\n`
-    let licenseData = `## License\n\nThis project is licensed uder the ${data[1].license} license.\n\n\n`
+    let usageData = `## Usage:\n\n${data.repo1}\n\n\n`
+    
+    let contributingData = `## Contributing:\n\n${data.repo2}\n\n\n`
+    let licenseData = `## License\n\nThis project is licensed uder the ${data.license} license.\n\n\n`
     
     let testData = `## Tests\n\nTo run tests, run the following command\n\n\n 
-    ${data[0].test}
+    ${data.test}
 
     \n\n\n`
     
-    let questionData = `## Questions\n\nIf you have any questions about the repo, open an issue or contact me directly at ${data[0].email}. You can find more of my work at [${data[0].userName}](https://github.com/${data[0].userName})`
+    let questionData = `## Questions\n\nIf you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.userName}](https://github.com/${data.userName})`
    
    
    
-    fs.writeFileSync('README.md',`# ${data[0].title}\nby: ${data[0].name}\n\n\n`, 'utf8')
+    fs.writeFileSync('README.md',`# ${data.title}\nby: ${data.name}\n\n\n![GitHub license](https://img.shields.io/badge/javascript-100%25-blue)\n\n\n`, 'utf8')
     fs.appendFileSync('README.md',descriptionData )
     fs.appendFileSync('README.md', tableofConents)
     fs.appendFileSync('README.md',installData)  
     fs.appendFileSync('README.md',usageData) 
-    fs.appendFileSync('README.md',licenseData) 
+    fs.appendFileSync('README.md',licenseData)
+    fs.appendFileSync('README.md', contributingData) 
     fs.appendFileSync('README.md',testData) 
     fs.appendFileSync('README.md',questionData) 
-    fs.appendFileSync('README.md', contributingData)
+    
 
 }
 
 // function to initialize program
 async function main() {
     let setup= await inquirer.prompt(questions);
-    userList.push({name: setup.name, email:setup.email,title:setup.projectName,
-    description:setup.description, userName:setup.userName, 
-    repo1: setup.usingRepo, repo2: setup.contributingRepo,install:setup.install,test:setup.test})
-    let choice = questions[5].choices
+    //loop through the options for licenses to push the one the user chose into userList
+    var choice = questions[5].choices
     for(i=0; i<choice.length;i++){
         if(setup.license===choice[i]){
-            userList.push({license:setup.license})
-
+            choice = setup.license
         }
 
     }
+    //pushing all the user inputs into userList
+    userList.push({name: setup.name, email:setup.email,title:setup.projectName,
+        description:setup.description, userName:setup.userName, 
+        repo1: setup.usingRepo, repo2: setup.contributingRepo,install:setup.install,test:setup.test, license:choice})
+
     console.log(userList)
-   writeToFile(userList)
 
+    //calling writetoFile to write the file with the information from userList.
+    //since userList is an object with in array have to call userList[0].
+   writeToFile(userList[0])
 
+   console.log('Generating README.....')
+   console.log('Completed!')
 }
-console.log(userList)
+
+
 
 // function call to initialize program
 main();
